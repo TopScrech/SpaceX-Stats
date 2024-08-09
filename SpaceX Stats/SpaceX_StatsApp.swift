1cca73b3593ca93b28988fbcc23145f4c68ec41d
TopScrech <sergei_saliukov@icloud.com>
2024-08-10 00:42:26 +0200

    Initial Commit

---
+++ SpaceX Stats/SpaceX_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_StatsApp.swift
+//  SpaceX Stats
+//
+//  Created by Sergei Saliukov on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
